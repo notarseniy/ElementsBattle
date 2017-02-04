7068bf64b13ce70494217e77d0ecce7292ae1c82
Arseniy Maximov <localhost@kern0.ru>
2017-02-04 04:13:57 +0300

    new static

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, './static'),
     filename: 'bundle.js',
-    publicPath: 'http://' + host + ':' + port + '/'
+    //publicPath: 'http://' + host + ':' + port + '/ElementsBattle/'
   },
   module: {
     rules: [
